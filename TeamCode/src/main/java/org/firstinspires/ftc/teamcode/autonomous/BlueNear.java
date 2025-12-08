5ed11af9ac9e6fb872169f8288bc6ab18afad3d3
ranouttauser <rico.alexander923@gmail.com>
2025-12-08 00:07:08 -0500

    making sure im doing this right

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueNear.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+// testing if im publishing this right
+@Autonomous(name = "Auto Proto", group = "Auto Test")
+public class BlueNear extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
